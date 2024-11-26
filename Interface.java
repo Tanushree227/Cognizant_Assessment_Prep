3667fde0ed9eccd191c01ec4ed73bbcbf4d7ea82
Tanushree227 <tanushreedas813@gmail.com>
2024-11-26 13:12:18 +0530

    Interface in Java

---
+++ Interface.java
@@ -0,0 +1,19 @@
+interface In1
+{
+    public static final int a = 2207;
+
+    void display();
+}
+
+public class Interface implements In1 {
+    public void display()
+    {
+        System.out.println("Interface in Java");
+    }
+
+    public static void main(String[] args) {
+        Interface obj = new Interface();
+        obj.display();
+        System.out.println(obj.a);
+    }
+}
